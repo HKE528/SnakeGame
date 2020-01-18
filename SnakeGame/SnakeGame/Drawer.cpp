61f326bee4cac9e675bcf01b14953690db407a01
Kieun <gosumo12@gmail.com>
2020-01-18 18:14:06 +0900

    gameBoard 변수추가
    Draw함수 추가

--- SnakeGame/SnakeGame/Drawer.cpp
+++ SnakeGame/SnakeGame/Drawer.cpp
@@ -1,2 +1,13 @@
 #include"Snake.h"
 
+void Draw()
+{
+	for (int i = 0; i < FRAME_HEIGHT; i++) {
+		for (int j = 0; j < FRAME_WIDTH; j++) {
+			if (gameBoard[i][j] = 1) {
+				Gotoxy(PADDING + j * 2, PADDING + i);
+				printf("бр")
+			}
+		}
+	}
+}
--- SnakeGame/SnakeGame/Snake.h
+++ SnakeGame/SnakeGame/Snake.h
@@ -4,11 +4,13 @@
 
 #define FRAME_WIDTH 40
 #define FRAME_HEIGHT 30
+#define PADDING 2
+
+int gameBoard[FRAME_HEIGHT][FRAME_WIDTH] = { 0 };
 
 //ConsolControl.cpp
 void SetConsol();
 void Gotoxy(int x, int y);
-
 
 //UI.cpp
 void GameUI();
